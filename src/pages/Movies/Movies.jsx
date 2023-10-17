35b443170291e6d86827754b8174077a18645c84
scribeNcode <deyeye99@gmail.com>
2023-10-17 22:06:58 +0100

    styled moviespage layout and added react icons

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -1,7 +1,31 @@
-import React  from "react";
+import React from "react";
+import "../Movies/Movies.css";
+import Logo from "./assets/Tv.png";
+import {GoHome} from 'react-icons/go'
+import {BsCameraReels} from 'react-icons/bs'
+import {PiMonitorPlayLight} from 'react-icons/pi'
+import {VscCalendar} from 'react-icons/vsc'
 function Movies() {
   return (
-    <div>Movies</div>
-  )
+    <div className="moviesPageWrapper">
+      <div className="moviesSideBarArea">
+        <div className="moviesSideBarcontainer">
+          <div className="moviesLogoContainer">
+            <object data={Logo}></object>
+            <h1>MovieBox</h1>
+          </div>
+
+          <ul className="moviesNavigationsContainer">
+            <li> <span className="moviesNavigationIcon">< GoHome/></span> <span id="goHomeName" className="moviesNavigationName" >Home</span></li>
+          <li> <span className="moviesNavigationIcon"><BsCameraReels/></span>  <span id="moviesHomeName" className="moviesNavigationName" >Movies</span> </li>
+          <li> <span className="moviesNavigationIcon"><PiMonitorPlayLight/></span> <span className="moviesNavigationName" >Tv series</span></li>
+          <li> <span className="moviesNavigationIcon"><VscCalendar/></span>  <span className="moviesNavigationName" >Upcoming</span> </li>
+          </ul>
+
+        </div>
+      </div>
+      <div className="moviesPageViewArea">mainview</div>
+    </div>
+  );
 }
-export default Movies
+export default Movies;
