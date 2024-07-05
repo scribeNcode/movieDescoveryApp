25b4ab8c6674bdd30f99df33c1d34ab6122c2d22
scribeNcode <deyeye99@gmail.com>
2024-07-05 15:04:38 +0100

    update all

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,8 +1,41 @@
+// import "./Home.css"
+// import Header from "../../components/Header/Header"
+// import Card from "../../components/Card/Card"
+
+// function Home() {
+//   return (
+//     <div className="homePage_container">
+//       <Header/>
+//       <Card/>
+//         {/* <div className="headerCardWrapper el1"><Header/></div>
+//          <div className="FeaturedMovieWrapper el2">
+//            <Card/>
+//           </div>  */}
+          
+//     </div>
+//   )
+// }
+// export default Home
+
+
 import "./Home.css"
 import Header from "../../components/Header/Header"
 import Card from "../../components/Card/Card"
+import React, { useState } from "react";
 
-function Home() {
+function Loader(){
+  return(
+    <section className="loaderSection">
+      <h1>MovieDescovery...</h1>
+    </section>
+    
+  )
+}
+
+
+
+
+function Content() {
   return (
     <div className="homePage_container">
       <Header/>
@@ -15,9 +48,28 @@ function Home() {
     </div>
   )
 }
+
+
+function Home() {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  setTimeout(()=>{
+    setIsLoading(false)
+  },5000)
+
+  return (
+    <section className="Home">
+       {isLoading ? <Loader/> : <Content/>}
+       <Loader/>
+    </section>
+   
+  );
+}
+
+
+
 export default Home
-
-
 
 
 
